b898ec7fea4054a52261e6a073596ec2589b1d2f
Ammar Mohammad <a.alkad.contact@gmail.com>
2026-08-11 21:22:32 +0300

    Add waving movement code

---
+++ Mechanical/Task-6-Three-Movement/files/code/waving.cpp
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo frontLeft;
+Servo frontRight;
+Servo rearLeft;
+Servo rearRight;
+
+// Neutral positions
+int frontLeftNeutral  = 90;
+int frontRightNeutral = 95;
+int rearLeftNeutral   = 95;
+int rearRightNeutral  = 95;
+
+// Waving positions
+int wavePosition1 = 70;
+int wavePosition2 = 120;
+
+void neutralPosition() {
+  frontLeft.write(frontLeftNeutral);
+  frontRight.write(frontRightNeutral);
+  rearLeft.write(rearLeftNeutral);
+  rearRight.write(rearRightNeutral);
+
+  delay(500);
+}
+
+void wave() {
+
+  // Keep the other three legs steady
+  frontLeft.write(frontLeftNeutral);
+  rearLeft.write(rearLeftNeutral);
+  rearRight.write(rearRightNeutral);
+
+  // Wave using the front-right leg
+  for (int i = 0; i < 5; i++) {
+
+    frontRight.write(wavePosition1);
+    delay(300);
+
+    frontRight.write(wavePosition2);
+    delay(300);
+  }
+
+  // Return front-right leg to neutral
+  frontRight.write(frontRightNeutral);
+
+  delay(500);
+}
+
+void setup() {
+
+  frontLeft.attach(3);
+  frontRight.attach(4);
+  rearLeft.attach(6);
+  rearRight.attach(7);
+
+  neutralPosition();
+
+  // Time to position the robot
+  delay(3000);
+}
+
+void loop() {
+
+  wave();
+
+  // Pause before repeating
+  delay(3000);
+}
